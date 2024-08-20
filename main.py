6436301b24b47fbc4b1f85a945767c01090f2559
mingzhaozhounephos <mz@nephos-solutions.co.uk>
2024-08-20 03:55:23 +0100

    Create columns, image, and description

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('images/photo.png')
+
+with col2:
+    st.title('Ardit Sulce')
+    content = """
+    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+    """
+    st.info(content)
+
